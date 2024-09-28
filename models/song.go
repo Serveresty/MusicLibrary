517d0648ffbbfddadfe2039b55384aef7770155d
Serveresty <cs.go.12228@gmail.com>
2024-09-28 14:49:04 +0300

    song's models

--- models/song.go
+++ models/song.go
@@ -1,1 +1,12 @@
 package models
+
+type SongRequest struct {
+	Group string `json:"group"`
+	Song  string `json:"song"`
+}
+
+type SongDetail struct {
+	ReleaseDate string `json:"releaseDate"`
+	Text        string `json:"text"`
+	Link        string `json:"link"`
+}
