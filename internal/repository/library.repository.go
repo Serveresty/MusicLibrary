394785139b90e5993472517def71770c2ca8c65b
Serveresty <cs.go.12228@gmail.com>
2024-09-30 21:57:48 +0300

    get lib and create song db methods

--- internal/repository/library.repository.go
+++ internal/repository/library.repository.go
@@ -1,26 +1,49 @@
 package repository
 
 import (
+	"MusicLibrary/models"
 	"MusicLibrary/pkg/logger"
+	"context"
 
 	"github.com/jackc/pgx/v4"
 )
 
 type LibraryRepository struct {
-	DB   *pgx.Conn
+	db   *pgx.Conn
 	Logs *logger.Loggers
 }
 
 func NewLibraryRepository(db *pgx.Conn, loggers *logger.Loggers) *LibraryRepository {
-	return &LibraryRepository{DB: db, Logs: loggers}
+	return &LibraryRepository{db: db, Logs: loggers}
 }
 
-func (lc *LibraryRepository) Create() {
-
+func (lc *LibraryRepository) Create(sr models.SongRequest, sd models.SongDetail) error {
+	_, err := lc.db.Exec(context.Background(), `INSERT INTO "songs" ("group", "song", "release_date", "text", "link") VALUES ($1,$2,$3,$4,$5)`,
+		sr.Group, sr.Song, sd.ReleaseDate, sd.Text, sd.Link)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func (lc *LibraryRepository) GetSongsLibrary() {
+func (lc *LibraryRepository) GetSongsLibrary(minID, limit string) ([]models.Song, error) {
+	rows, err := lc.db.Query(context.Background(), `SELECT * FROM "songs" WHERE id >= $1 LIMIT $2`, minID, limit)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
+	var songs []models.Song
+	for rows.Next() {
+		var song models.Song
+		if err := rows.Scan(&song.ID, &song.Group, &song.Song, &song.ReleaseDate, &song.Text, &song.Link); err != nil {
+			return nil, err
+		}
+
+		songs = append(songs, song)
+	}
+
+	return songs, nil
 }
 
 func (lc *LibraryRepository) GetSongText() {
