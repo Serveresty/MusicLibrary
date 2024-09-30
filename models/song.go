4af31de0c10c469c936567825432fa2e2908cf15
Serveresty <cs.go.12228@gmail.com>
2024-09-30 21:57:27 +0300

    full song model

--- models/song.go
+++ models/song.go
@@ -1,5 +1,14 @@
 package models
 
+type Song struct {
+	ID          int    `json:"id"`
+	Group       string `json:"group"`
+	Song        string `json:"song"`
+	ReleaseDate string `json:"releaseDate"`
+	Text        string `json:"text"`
+	Link        string `json:"link"`
+}
+
 type SongRequest struct {
 	Group string `json:"group"`
 	Song  string `json:"song"`
