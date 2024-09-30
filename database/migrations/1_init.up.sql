e7528dd46ddb80b68bea34b230be2dc42bdecf9c
Serveresty <cs.go.12228@gmail.com>
2024-09-30 21:58:50 +0300

    replaced DATE type to VARCHAR

--- database/migrations/1_init.up.sql
+++ database/migrations/1_init.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS "songs" (
     "id" SERIAL PRIMARY KEY,
     "group" VARCHAR(255) NOT NULL,
     "song" VARCHAR(255) NOT NULL,
-    "release_date" DATE,
+    "release_date" VARCHAR(255),
     "text" VARCHAR(255),
     "link" VARCHAR(255)
 );
