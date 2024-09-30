016c25b3efcfe37597123450ae32cfef660f2d11
Serveresty <cs.go.12228@gmail.com>
2024-09-30 23:26:36 +0300

    more info api req

--- internal/service/library.service.go
+++ internal/service/library.service.go
@@ -3,6 +3,11 @@ package service
 import (
 	"MusicLibrary/internal/repository"
 	"MusicLibrary/models"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
 )
 
 type LibraryService struct {
@@ -13,7 +18,20 @@ func NewLibraryService(repo *repository.LibraryRepository) *LibraryService {
 	return &LibraryService{Repo: repo}
 }
 
-func (ls *LibraryService) GetMoreInfo(info models.SongRequest) (models.SongDetail, int) {
+func (ls *LibraryService) GetMoreInfo(info models.SongRequest) (models.SongDetail, int, error) {
+	api := os.Getenv("API_URL")
+	url := fmt.Sprintf(api+"?group=%s&song=%s", url.QueryEscape(info.Group), url.QueryEscape(info.Song))
 
-	return models.SongDetail{}, 200
+	resp, err := http.Get(url)
+	if err != nil {
+		return models.SongDetail{}, http.StatusInternalServerError, err
+	}
+	defer resp.Body.Close()
+
+	var songDetail models.SongDetail
+	if err := json.NewDecoder(resp.Body).Decode(&songDetail); err != nil {
+		return models.SongDetail{}, http.StatusBadRequest, err
+	}
+
+	return songDetail, http.StatusOK, nil
 }
