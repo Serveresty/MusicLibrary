8d2751e5ee1c348a54b963da472a2b3095cf9218
Serveresty <cs.go.12228@gmail.com>
2024-10-02 18:57:57 +0300

    reworked logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,18 +1,48 @@
 package logger
 
 import (
-	"log"
+	"context"
+	"log/slog"
 	"os"
 )
 
 type Loggers struct {
-	Info  *log.Logger
-	Debug *log.Logger
+	Info  *slog.Logger
+	Debug *slog.Logger
 }
 
-func NewLoggers() *Loggers {
-	return &Loggers{
-		Info:  log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile),
-		Debug: log.New(os.Stdout, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile),
+func NewLoggers() (*Loggers, error) {
+	infoHandler, err := newFileHandler("./logs/info.json")
+	if err != nil {
+		return nil, err
 	}
+
+	debugHandler, err := newFileHandler("./logs/debug.json")
+	if err != nil {
+		return nil, err
+	}
+
+	infoLogger := slog.New(infoHandler)
+	debugLogger := slog.New(debugHandler)
+
+	return &Loggers{
+		Info:  infoLogger,
+		Debug: debugLogger,
+	}, nil
+}
+
+func newFileHandler(filePath string) (*slog.JSONHandler, error) {
+	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		return nil, err
+	}
+	return slog.NewJSONHandler(file, nil), nil
+}
+
+func (l *Loggers) InfoLog(msg string, attrs ...slog.Attr) {
+	l.Info.LogAttrs(context.Background(), slog.LevelInfo, msg, attrs...)
+}
+
+func (l *Loggers) DebugLog(msg string, attrs ...slog.Attr) {
+	l.Debug.LogAttrs(context.Background(), slog.LevelDebug, msg, attrs...)
 }
